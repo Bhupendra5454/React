0605940cb63372ff5e6ff25085f9634acbcf0d8e
Bhupebdra5454 <akshayw2512@gmail.com>
2024-08-23 15:26:31 +0530

    Route.js

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,57 +1,18 @@
-import logo from './logo.svg';
-import Home from './Main/Components/Home';
-import About from './Main/Components/About';
-import Contact from './Main/Components/Contact';
-import Tap from './Main/Components/Tap';
-
-import Login from './Membership/Components/Login';
-import Register from './Membership/Components/Register';
-
-import List from './Catalog/Components/List';
-import Details from './Catalog/Components/Details';
-
-import { BrowserRouter as Router,Link,Route,Routes } from 'react-router-dom';
-import Dashboard from './BI/Components/Dashboard';
-import LineChart from './BI/Components/Charts/LineChart';
-
-import BarChart from './BI/Components/Charts/BarChart';
-import PieChart from './BI/Components/Charts/PieChart';
-
+import { BrowserRouter as Router} from 'react-router-dom';
+import NavBar from './Main/Components/NavBar';
+import AppRoutes from './Routes/Routes';
 
 function App() {
   return (
     <div>
-
     <div class="container-fluid p-5 bg-primary text-white text-center">
       <h1>Transflower</h1>
-      <p>A learning experience!</p> 
     </div>
-      
     <div class="container mt-5">
       <Router>
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/products">Flowers</Link>|
-          <Link to="/dashboard">Dashboard</Link>|<Link to="/about">About</Link>|
-          <Link to="/contact">Contact</Link>|<Link to="/login">Login</Link> 
-          <Link to="/register">Register</Link> |<Link to="/tap">Tap</Link>
-        </nav>
-        <hr/>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="tap" element={<Tap />} />
-          <Route path="products" element={<List />}/>
-          <Route path="products/:productId" element={<Details />} />
-          <Route path="dashboard" element={<Dashboard />}>
-                 {/* Nested Routing   */}
-                   <Route path="line" element={<LineChart/>}></Route>
-                  <Route path="bar" element={<BarChart/>}></Route>
-                  <Route path="pie" element={<PieChart/>}></Route>
-          </Route>
-        </Routes>
+          <NavBar />
+          <hr/>
+          <AppRoutes />
       </Router>
     </div>
 </div>
--- testapp/src/BI/Components/Dashboard.js
+++ testapp/src/BI/Components/Dashboard.js
@@ -3,17 +3,12 @@ const Dashboard =()=>{
     return(
             <div>
                 <h3>Dashboard</h3>
-                  
-                <nav>
-                    <Link to="/dashboard/line">Line Chart</Link>| <Link to="/dashboard/bar">Bar Chart</Link>| <Link to="/dashboard/pie">Pie Chart</Link>
-                </nav>
+                    <nav>
+                        <Link to="/dashboard/line">Line Chart</Link>| <Link to="/dashboard/bar">Bar Chart</Link>| <Link to="/dashboard/pie">Pie Chart</Link>
+                    </nav>
                     <Outlet/>
-                    
-                  
-                { /* this will render nested routes  */}
+                    { /* this will render nested routes  */}
             </div>
-
     );
 }
-
 export default Dashboard;
